0120e7c86172234d6f93ac4ba4fb5d60e3a66974
The-Assembly <32713072+The-Assembly@users.noreply.github.com>
2018-02-01 13:08:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const socket = io();
 
 const outputYou = document.querySelector('.output-you');
@@ -40,16 +38,4 @@ recognition.addEventListener('error', (e) => {
   outputBot.textContent = 'Error: ' + e.error;
 });
 
-function synthVoice(text) {
-  const synth = window.speechSynthesis;
-  const utterance = new SpeechSynthesisUtterance();
-  utterance.text = text;
-  synth.speak(utterance);
-}
 
-socket.on('bot reply', function(replyText) {
-  synthVoice(replyText);
-
-  if(replyText == '') replyText = '(No answer...)';
-  outputBot.textContent = replyText;
-});
